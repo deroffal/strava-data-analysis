7d1206f50e6e5259cd204289af2d514a2b06b75f
deroffal <32491733+deroffal@users.noreply.github.com>
2024-02-12 21:18:29 +0100

    rename collection with Strava data to strava_*

--- strava-statistics/src/main/java/fr/deroffal/stravastatistics/repository/ActivityDocument.java
+++ strava-statistics/src/main/java/fr/deroffal/stravastatistics/repository/ActivityDocument.java
@@ -5,7 +5,7 @@ import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document(collection = "activity")
+@Document(collection = "strava_activity")
 public class ActivityDocument extends DetailedActivity {
 
   @Id
--- strava-statistics/src/main/java/fr/deroffal/stravastatistics/repository/SummaryDocument.java
+++ strava-statistics/src/main/java/fr/deroffal/stravastatistics/repository/SummaryDocument.java
@@ -5,7 +5,7 @@ import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document(collection = "summary")
+@Document(collection = "strava_summary")
 public class SummaryDocument extends SummaryActivity {
 
   @Id
